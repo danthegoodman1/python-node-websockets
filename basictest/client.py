76975201de5b5f4d3de0001f2efdec4d9b817740
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-04-18 10:31:20 -0400

    Update client.py

--- basictest/client.py
+++ basictest/client.py
@@ -3,6 +3,7 @@
 import asyncio
 import websockets
 import json
+from time import sleep
 
 async def hello(uri):
     async with websockets.connect(uri) as websocket:
@@ -15,8 +16,21 @@ async def test2(uri):
         greeting = json.loads(greeting)
         print("greeting " + greeting['desc'])
 
+async def longwait(uri):
+    async with websockets.connect(uri) as ws:
+        while True:
+            sleep(2)
+            greeting = await ws.recv()
+            print("greeting " + greeting)
+        print("Outside the loop")
+
+# asyncio.get_event_loop().run_until_complete(
+#     hello('ws://socketio-tutorial-dgoodman.c9users.io:80'))
+# asyncio.get_event_loop().run_until_complete(
+#     test2('ws://socketio-tutorial-dgoodman.c9users.io:80')
+# )
+# So since this is async now I think we might just make one master connection and have the events triggered inside
 asyncio.get_event_loop().run_until_complete(
-    hello('ws://socketio-tutorial-dgoodman.c9users.io:80'))
-asyncio.get_event_loop().run_until_complete(
-    test2('ws://socketio-tutorial-dgoodman.c9users.io:80')
+    longwait('ws://socketio-tutorial-dgoodman.c9users.io:80')
 )
+print("done!")
