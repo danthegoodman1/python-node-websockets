5584ee3bafdd411dfa860b722d47b4b7266a3701
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-04-18 09:26:42 -0400

    Update index.js

--- basictest/index.js
+++ basictest/index.js
@@ -8,8 +8,7 @@ console.log("Websocket server listening on: " + wsPort);
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send("hello");
   });
 
-  ws.send('HEYEYEYEY');
+  ws.send(JSON.stringify({'desc': "helloooooooo"}));
 });
