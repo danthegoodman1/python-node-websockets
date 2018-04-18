bc683bf769e8e316ba1f6dc7c84f966fa874ea08
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-04-18 09:26:28 -0400

    Update client.py

--- basictest/client.py
+++ basictest/client.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import websockets
+import json
 
 async def hello(uri):
     async with websockets.connect(uri) as websocket:
@@ -11,7 +12,8 @@ async def test2(uri):
     async with websockets.connect(uri) as websocket:
         await websocket.send("Here is fun!")
         greeting = await websocket.recv()
-        print("greeting " + greeting)
+        greeting = json.loads(greeting)
+        print("greeting " + greeting['desc'])
 
 asyncio.get_event_loop().run_until_complete(
     hello('ws://socketio-tutorial-dgoodman.c9users.io:80'))
